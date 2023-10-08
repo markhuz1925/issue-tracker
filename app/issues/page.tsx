f9a9edaca504698cecdbb5ab05b1f74a643200ce
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-08 21:31:48 +0800

    Setup Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@radix-ui/themes";
 export default function IssuesPage() {
-  return <div>Issues Page</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 }
