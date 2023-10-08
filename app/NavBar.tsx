31bc736f1e92dd8c5998a16f538a85d09cf62aa7
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-08 20:17:12 +0800

    Navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+
+export default function NavBar() {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex items-center space-x-6 border-b mb-5 px-5 h-14">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link, index) => (
+          <li key={index}>
+            <Link
+              href={link.href}
+              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
