c40df622b62d0a17a2f2861df692b2698f92253f
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-10 02:11:38 +0800

    Improve the loading experience.

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,10 +1,3 @@
-import { Skeleton } from "@/app/components";
-import { Box } from "@radix-ui/themes";
-export default function LoadingNewIssuePage() {
-  return (
-    <Box className="max-w-xl">
-      <Skeleton />
-      <Skeleton height="20rem" />
-    </Box>
-  );
-}
+import IssueFormSkeleton from "../_components/IssueFormSkeleton";
+
+export default IssueFormSkeleton;
